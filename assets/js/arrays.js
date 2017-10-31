2600ed6cfc5932c61aee08ed494340d56b2f9bd3
BrenoOliveira <brenoo194@gmail.com>
2017-10-31 20:04:30 -0200

    aula de arrays

---
+++ assets/js/arrays.js
@@ -0,0 +1,11 @@
+window.onload = function (){
+    var nome = ['breno','bruna','tesste','teste'];
+    console.log (nome.splice(0,3));
+    
+    nome.push('joaozinho');
+    
+    console.log(nome);
+    nome.pop ('breno');
+    
+    
+};
