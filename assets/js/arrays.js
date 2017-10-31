4a61f39fabd40cb2437a41e83670ba16521a190f
BrenoOliveira <brenoo194@gmail.com>
2017-10-31 20:21:15 -0200

    loops

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -9,3 +9,32 @@ window.onload = function (){
     
     
 };
+//existe uma maneiro de criar um array, instancioando o new array
+//FOREARCH
+window.onload = function (){
+    var nome = ['breno','bruna','tesste','teste'];
+    nome.forEach(function(nome){
+        //cria passa uma variavel por referencia somente para usar no log
+        console.log(nome);
+    });
+    
+    
+    console.log(nome.length);
+    
+    //FOR
+    
+    for (i=0;i<nome.length;i++){
+        console.log(nome[i]);
+        
+    }
+    
+    
+    //WHILE
+    var totalNomes= nome.length;
+    var i = 0;
+    while (i<totalNomes){
+        console.log(nome[i]);
+        i++;
+        
+    }
+};
